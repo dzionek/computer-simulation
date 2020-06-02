7734097bb394fc6cc3202b27d2076650cff3dfd8
dzionek <vburtoch@gmail.com>
2020-06-02 13:30:54 +0200

    Improve UI in decay/main.py

--- decay/main.py
+++ decay/main.py
@@ -1,14 +1,11 @@
 from decay import Decay
 
+"""
+Module runs the simulation.
+"""
+
 # Decay constant of the nucleide in the assignment
 ACTUAL_CONSTANT = 24.98
-
-"""
-Suggested values for the given example (Iodine-128) are
-decay const := 0.02775 min^-1
-size := 50
-timestep := 0.01 min
-"""
 
 def main() -> None:
     """Main function printing basic and extra decay scripts"""
@@ -21,6 +18,14 @@ def print_basic() -> None:
     2) Find the half-time, and print the matrix with data about decayed nuclei.
     3) Compare the simulated half-time with the actual half-time.
     """
+    print("""This is Monte Carlo simulation of a radioactive decay.
+    
+Provide the necessary constants. For example, for Iodine-128 they are:
+* decay const := 0.02775 min^-1
+* size := 50
+* timestep := 0.01 min
+""")
+
     decay_const = float(input("Provide the decay constant [min^-1]: "))
     size = int(input("Provide the size of the NxN array: "))
     timestep = float(input("Provide the timestep of the decay [min]: "))
@@ -38,7 +43,7 @@ Legend: 0 - decayed; 1 - undecayed
 {nucleide}
 The model started with {initial_undecayed} nuclei. After the decay {final_undecayed} of them remained.
 The simulation found that the half-time of this nucleide is approximately {half_time}min.
-The actual half-time is {ACTUAL_CONSTANT}min.
+The actual half-time of Iodine-128 is {ACTUAL_CONSTANT}min.
 """)
 
 
